ea78a70e609cf2755348e17f88fc1d5e58cc04bf
Murray Scott <rs06gms@gmail.com>
2023-02-18 15:39:34 +0000

    modified the app.py for blueprints

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask
 
+from controllers.supplier_controller import suppliers_blueprint
+from controllers.product_controller import products_blueprint
+from controllers.manufacturer_controller import manufacturers_blueprint
+
 app = Flask(__name__)
 
-from controllers import controller
+app.register_blueprint(suppliers_blueprint)
+app.register_blueprint(products_blueprint)
+app.register_blueprint(manufacturers_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
