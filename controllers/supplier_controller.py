34e5079dfbfcb9ddde074298209e76c223936e55
Murray Scott <rs06gms@gmail.com>
2023-02-21 13:08:02 +0000

    added new CRUD routes to supplier_controller

--- controllers/supplier_controller.py
+++ controllers/supplier_controller.py
@@ -11,3 +11,31 @@ suppliers_blueprint = Blueprint("suppliers", __name__)
 def suppliers():
     suppliers = supplier_repository.select_all() # NEW
     return render_template("suppliers/index.html", suppliers = suppliers)
+
+# NEW
+# GET '/suppliers/new'
+@suppliers_blueprint.route("/suppliers/new", methods=['GET'])
+def new_task():
+    products = product_repository.select_all()
+    manufacturers = manufacturer_repository.select_all()
+    return render_template("suppliers/new.html", product =products, manufacturers = manufacturers)
+
+# CREATE
+# POST '/suppliers'
+@suppliers_blueprint.route("/suppliers",  methods=['POST'])
+def create_task():
+    product_id = request.form['product_id']
+    manufacturer_id = request.form['manufacturer_id']
+    product = product_repository.select(product_id)
+    manufacturer =  manufacturer_repository.select(manufacturer_id)
+    supplier = Supplier(product, manufacturer)
+    supplier_repository.save(supplier)
+    return redirect('/suppliers')
+
+
+# DELETE
+# DELETE '/suppliers/<id>'
+@suppliers_blueprint.route("/supplier/<id>/delete", methods=['POST'])
+def delete_task(id):
+    supplier_repository.delete(id)
+    return redirect('/visits')
