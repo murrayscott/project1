d38d61eee2fbd9ef0df5d92129ef78e38181dd54
Murray Scott <rs06gms@gmail.com>
2023-02-21 13:18:09 +0000

    added new routes to product_controller

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -7,5 +7,11 @@ products_blueprint = Blueprint("products", __name__)
 
 @products_blueprint.route("/products")
 def products():
-    products = product_repository.select_all() # NEW
+    products = product_repository.select_all()
     return render_template("products/index.html", products = products)
+
+@products_blueprint.route("/products/<id>")
+def show(id):
+    product = product_repository.select(id)
+    manufacturers = product_repository.manufacturers(product)
+    return render_template("products/show.html", product = product , manufacturers = manufacturers)
